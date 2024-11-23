3a9bcf361c860c69e935a0e30094818fe448d658
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2024-11-23 00:12:15 -0500

    anadir post en costos

--- src/app/components/costos/costos.component.ts
+++ src/app/components/costos/costos.component.ts
@@ -1,32 +1,62 @@
 import { Component } from '@angular/core';
 import { CostosService } from '../../services/costos.service';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-costos',
-  standalone: false,
   templateUrl: './costos.component.html',
   styleUrl: './costos.component.css'
 })
 export class CostosComponent {
-  costosList: any = [];
+  costosList: any = []; // Cambiado 'ingresosList' por 'costosList'
+  costosForm: any = this.formBuilder.group({
+    nombre: '',
+    descripcion: '',
+    monto: '',
+    fecha: Date
+  })
+  editableCostos: boolean = false;
+  idCostos: any;
 
-  constructor(private costosService: CostosService) { }
+  constructor(private CostosService: CostosService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
-    this.getAllCostos();
+    this.getAllCostos(); // Cambiado 'getAllIngresos' por 'getAllCostos'
   }
 
-
-  getAllCostos() {
-    this.costosService.getAllCostosData().subscribe((data: {}) => {
-      this.costosList = data;
+  getAllCostos() { // Cambiado 'getAllIngresos' por 'getAllCostos'
+    this.CostosService.getAllCostosData().subscribe((data: {}) => {
+      this.costosList = data; // Cambiado 'ingresosList' por 'costosList'
     });
   }
 
-  deleteCostoEntry(id: any) {
-    console.log(`Eliminando Costo con ID: ${id}`); // Se agrega un comentario indicando que se va a eliminar el Costo
-    this.costosService.deleteCosto(id).subscribe(() => {
-      console.log(`Costo con ID: ${id} eliminado exitosamente.`); // Confirmación de eliminación exitosa
+  newMessages(messageText: string) {
+    this.toastr.success('Clic aquí para actualizar la lista', messageText)
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => window.location.reload());
+  }
+
+  newCostoEntry() { // Cambiado 'newIngresoEntry' por 'newCostoEntry'
+    this.CostosService.newCosto(localStorage.getItem('accessToken'), this.costosForm.value).subscribe(
+      () => {
+        this.router.navigate(['/costos']).then(() => { // Cambiado '/ingresos' por '/costos'
+          this.newMessages('Registro exitoso');
+        })
+      }
+    );
+  }
+
+  deleteCostoEntry(id: any) { // Cambiado 'deleteIngresoEntry' por 'deleteCostoEntry'
+    console.log(`Eliminando Costo con ID: ${id}`); // Cambiado 'Ingreso' por 'Costo'
+    this.CostosService.deleteCosto(id).subscribe(() => { // Cambiado 'ingresosService' por 'CostosService'
+      console.log(`Costo con ID: ${id} eliminado exitosamente.`); // Cambiado 'Ingreso' por 'Costo'
     });
   }
 }
